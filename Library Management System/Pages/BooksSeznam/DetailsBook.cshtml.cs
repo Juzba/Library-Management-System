35a7adba9c2d80fc6dacecebf0bfc1e6ac1e086c
Jiří Strnadel <Juzba88@gmail.com>
2025-05-22 02:43:56 +0200

    Working

---
+++ Library Management System/Pages/BooksSeznam/DetailsBook.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Library_Management_System.Data;
+using Library_Management_System.Models;
+
+namespace Library_Management_System.Pages.BooksSeznam
+{
+    public class DetailsBookModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DetailsBookModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (book is not null)
+            {
+                Book = book;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
--- Library Management System/Pages/BooksSeznam/Seznam.cshtml.cs
+++ Library Management System/Pages/BooksSeznam/Seznam.cshtml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Library_Management_System.Data;
+using Library_Management_System.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Library_Management_System.Data;
-using Library_Management_System.Models;
 
 namespace Library_Management_System.Pages.BooksSeznam
 {
@@ -19,11 +15,63 @@ namespace Library_Management_System.Pages.BooksSeznam
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string SearchText { get; set; } = "";
+        [BindProperty(SupportsGet = true)]
+        public string SearchBy { get; set; } = "";
+        public IList<Book> Book { get; set; } = default!;
+        static int lastId = 0;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
-            Book = await _context.Books.ToListAsync();
+            SearchText ??= "";
+            if (id is not null) lastId = id.Value;
+
+            switch (SearchBy)
+            {
+                case ("1"):
+                    Book = await _context.Books.Where(p => p.Title.Contains(SearchText)).ToListAsync();
+                    break;
+                case ("2"):
+                    Book = await _context.Books.Where(p => p.Id.ToString().Contains(SearchText)).ToListAsync();
+                    break;
+                case ("3"):
+                    Book = await _context.Books.Where(p => p.Author.Contains(SearchText)).ToListAsync();
+                    break;
+                case ("4"):
+                    Book = await _context.Books.Where(p => p.YearPublished.ToString().Contains(SearchText)).ToListAsync();
+                    break;
+                case ("5"):
+                    Book = await _context.Books.Where(p => p.Genre.Contains(SearchText)).ToListAsync();
+                    break;
+                default:
+                    Book = await _context.Books.Where(p => p.Title.Contains(SearchText)).ToListAsync();
+                    break;
+            }
+
+            switch (lastId)
+            {
+                case (0):
+                    Book = [.. Book.OrderBy(p => p.Id)];
+                    break;
+                case (1):
+                    Book = [.. Book.OrderBy(p => p.Title)];
+                    break;
+                case (2):
+                    Book = [.. Book.OrderBy(p => p.Author)];
+                    break;
+                case (3):
+                    Book = [.. Book.OrderBy(p => p.YearPublished)];
+                    break;
+                case (4):
+                    Book = [.. Book.OrderBy(p => p.Genre)];
+                    break;
+                default:
+                    break;
+            }
+
+            
+            return Page();
         }
     }
 }
